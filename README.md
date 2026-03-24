780d118b7ea4ae9571cadb421dfbe5899458604e
RasmusRaschke <rasmus.raschke@gmail.com>
2026-03-25 00:48:38 +0100

    added readme and corrected script, added analytical top

---
+++ README.md
@@ -0,0 +1,15 @@
+# Welcome!
+If you wonder where to find specific things, this readme should make things clear. I will go shortly over each of the folders.
+## [Code](code/)
+In this place, you find all the code I have written so far. 
+- The [archive](code/archive/) contains old code not needed at the moment.
+- The folder [cpp](code/cpp/) contains the up-to-date code. You can find [source code](code/cpp/src/) as well as many [examples](code/cpp/simulations/) in there.
+
+## [Inkscape](inkscape/)
+Here you can find the images I created with inkscape. Those are mostly overview images of the system's physics.
+
+## [Script](script/)
+This folder contains the complete theoretical background and all my derivations for the main system.
+
+## [Topology](topology/)
+This contains a quick detour I did about the topology of the [sine surface](https://www.mathcurve.com/surfaces.gb/sinus/sinus.shtml) and the [Seifert-van Kampen Theorem](https://en.wikipedia.org/wiki/Seifert%E2%80%93Van_Kampen_theorem).
--- code/cpp/simulations/FIGC_top/visualizer.py
+++ code/cpp/simulations/FIGC_top/visualizer.py
@@ -10,9 +10,26 @@ plt.rcParams.update({
     "ytick.labelsize": 14,
     "legend.fontsize": 14,
 })
-
-
-
+g = 9.80665
+B = 5.0e-5
+R = 0.005
+M = 0.004
+mu_init = [0,0.174,0.985]
+beta0 = 0.174533
+def mag_top(x0, y0, z0, mu, spin, t):
+    mu_norm = np.linalg.norm(mu)
+    omega0 = np.sqrt((5*B*mu_norm)/(7*R**2*M))
+    delta = np.sqrt(spin**2 + 4*omega0**2)
+    etap = 0.5 * (spin + delta)
+    etam = 0.5 * (spin - delta)
+    w0 = (mu[0] + 1j * mu[1]) / mu_norm
+    q = (x0 + 1j * y0) + (w0 * R / delta) * (- etam * (np.exp(1j*etap*t)-1) + etap * (np.exp(1j*etam*t) - 1))
+    x = np.real(q)
+    y = np.imag(q)
+    z = np.full_like(t, z0, dtype=float)
+    return x,y,z
+    
+'''
 ####################No Friction#############################
 data = np.genfromtxt('harm_osz.csv', delimiter=",", names=True, dtype=None, encoding=None)
 headers = data.dtype.names
@@ -94,7 +111,18 @@ x_cho_vgl, y_cho_vgl, z_cho_vgl = harm_osz(3,0,0,0,0,0,0, mu_init,540.0, 0.01, t
 x_oho_vgl, y_oho_vgl, z_oho_vgl = harm_osz(4,0,0,0,0,0,0, mu_init,5400.0, 0.01, t)
 
 x_dry_vgl, y_dry_vgl, z_dry_vgl = harm_osz(5,0,0,0,0,0,0, mu_init,0.002, 1 / R, t) #technically, k is 1 as it does not enter the dry model, but the dry model has an additional 1/R, so this compensates for that
+'''
+t = np.linspace(1,10,10000)
+x_test, y_test, z_test = mag_top(0,0,0,[0.1,0.1,1.0],np.deg2rad(1000),t)
 
+fig1, ax1 = plt.subplots(layout='constrained')
+ax1.plot(x_test*1000, y_test*1000, color='red', lw=1.0)
+ax1.grid(True)
+ax1.set_xlabel(r"$x \, [\text{mm}]$")
+ax1.set_ylabel(r"$y \, [\text{mm}]$")
+ax1.tick_params(which='both', bottom=True, top=True, left=True, right=True, labelbottom=True, labelleft=True, labelright=False, labeltop=False)
+plt.savefig("test.pdf", dpi=100)
+'''
 fig1, ax1 = plt.subplots(layout='constrained')
 ax1.plot(t, y_ho_vgl*1000, color='red', label=r"analytical", lw=3.5)
 ax1.plot(t, y_ho*1000, 'o-', markevery=400, color='black', ms=7, label=r"numerical")
@@ -107,83 +135,7 @@ ax1.set_ylim([-2.1,0.1])
 ax1.legend(loc='lower center', ncols=2)
 plt.savefig("FIGB1_harm_osz_vgl.pdf", dpi=100)
 
-fig2, ax2 = plt.subplots(layout='constrained')
-ax2.plot(t, y_uho_sth_vgl*1000, color='red', label=r"underdamped", lw=3.5)
-ax2.plot(t, y_dry_sth_vgl*1000, color='blue', label=r"dry", lw=3.5)
-ax2.plot(t, y_smooth*1000, 'o-', markevery=500, color='black', ms=7, lw=2, label=r"numerical")
-ax2.grid(True)
-ax2.set_xlabel(r"$t \, [\text{s}]$")
-ax2.set_ylabel(r"$y \, [\text{mm}]$")
-ax2.tick_params(which='both', bottom=True, top=True, left=True, right=True, labelbottom=True, labelleft=True, labelright=False, labeltop=False)
-ax2.set_xlim([0,3])
-ax2.set_ylim([-2.1,0.1])
-ax2.legend(loc='lower center', ncols=2)
-plt.savefig("FIGB2_smooth_harm_osz_vgl.pdf", dpi=100)
 
-fig3, ax3 = plt.subplots(layout='constrained')
-ax3.plot(t, y_uho_vgl*1000, color='red', label=r"analytical", lw=3.5)
-ax3.plot(t, ev1_uho_upper*1000, '--', color='red', label=r"envelope", lw=2)
-ax3.plot(t, y_uho*1000, 'o-', markevery=500, color='black', ms=7,lw=2, label=r"numerical")
-ax3.grid(True)
-ax3.set_xlabel(r"$t \, [\text{s}]$")
-ax3.set_ylabel(r"$y \, [\text{mm}]$")
-ax3.tick_params(which='both', bottom=True, top=True, left=True, right=True, labelbottom=True, labelleft=True, labelright=False, labeltop=False)
-ax3.set_xlim([0,3])
-ax3.set_ylim([-1.75,0.1])
-ax3.legend(loc='lower center', ncols=2)
-plt.savefig("FIGB3_udamp_harm_osz_vgl.pdf", dpi=100)
-
-fig4, ax4 = plt.subplots(layout='constrained')
-ax4.plot(t, y_cho_vgl*1000, color='red', label=r"analytical", lw=3.5)
-ax4.plot(t, y_cho*1000, 'o-', markevery=1000, color='black', ms=7,lw=2, label=r"numerical")
-ax4.grid(True)
-ax4.set_xlabel(r"$t \, [\text{s}]$")
-ax4.set_ylabel(r"$y \, [\text{mm}]$")
-ax4.tick_params(which='both', bottom=True, top=True, left=True, right=True, labelbottom=True, labelleft=True, labelright=False, labeltop=False)
-ax4.set_xlim([0,3])
-ax4.set_ylim([-1.1,0.1])
-ax4.legend(loc='lower center', ncols=2)
-plt.savefig("FIGB4_cdamp_harm_osz_vgl.pdf", dpi=100)
-
-fig5, ax5 = plt.subplots(layout='constrained')
-ax5.plot(t, y_oho_vgl*1000, color='red', label=r"analytical", lw=3.5)
-ax5.plot(t, y_oho*1000, 'o-', markevery=1000, color='black', ms=7,lw=2, label=r"numerical")
-ax5.grid(True)
-ax5.set_xlabel(r"$t \, [\text{s}]$")
-ax5.set_ylabel(r"$y \, [\text{mm}]$")
-ax5.tick_params(which='both', bottom=True, top=True, left=True, right=True, labelbottom=True, labelleft=True, labelright=False, labeltop=False)
-ax5.set_xlim([0,3])
-ax5.set_ylim([-1,0.1])
-ax5.legend(loc='lower left', ncols=2)
-plt.savefig("FIGB5_odamp_harm_osz_vgl.pdf", dpi=100)
-
-fig6, ax6 = plt.subplots(layout='constrained')
-ax6.plot(t, y_dry_vgl*1000, color='red', label=r"analytical", lw=3.5)
-ax6.plot(t, y_dry*1000, 'o-', markevery=1000, color='black', ms=7,lw=2, label=r"numerical")
-ax6.grid(True)
-ax6.set_xlabel(r"$t \, [\text{s}]$")
-ax6.set_ylabel(r"$y \, [\text{mm}]$")
-ax6.tick_params(which='both', bottom=True, top=True, left=True, right=True, labelbottom=True, labelleft=True, labelright=False, labeltop=False)
-ax6.set_xlim([0,3])
-ax6.set_ylim([-2,0.1])
-ax6.legend(loc='lower center', ncols=2)
-plt.savefig("FIGB6_dry_harm_osz_vgl.pdf", dpi=100)
-
-fig7, ax7 = plt.subplots(layout="constrained")
-ax7.plot(t, E_smooth*1e6, color='red', label=r"smoothed", lw=3.5)
-ax7.plot(t, E_uho*1e6, color='blue', label=r"underdamped", lw=3.5)
-ax7.plot(t, E_cho*1e6, color='aqua', label=r"critial", lw=3.5)
-ax7.plot(t, E_oho*1e6, color='darkblue', label=r"overdamped", lw=3.5)
-ax7.plot(t, E_dry*1e6, color='deeppink', label=r"dry", lw=3.5)
-ax7.set_xlabel(r"$t \, [\text{s}]$")
-ax7.set_ylabel(r"$E \, [\text{µJ}]$")
-ax7.tick_params(which='both', bottom=True, top=True, left=True, right=True, labelbottom=True, labelleft=True, labelright=False, labeltop=False)
-ax7.grid(True)
-ax7.set_xlim([0,3])
-ax7.legend(loc='upper right', ncols=2)
-plt.savefig("FIGB7_energies.pdf", dpi=100)
-
-'''
 fig2, ax2 = plt.subplots(layout='constrained')
 #ax2.plot(t, x_deg5, color='darkred', label=r"$\tilde{x}(t)$")
 ax2.plot(t5, y_deg5, color='grey', label=r"$\varphi = 5^\circ$", lw=3.5)
